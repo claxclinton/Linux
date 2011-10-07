4cfd8461643fb1d9b556fa0c911c57ae281325b5
Claes Lillieskold <claes.lillieskold@gmail.com>
2011-10-07 13:04:08 +0200

    Renamed clli_example_module to do_nothing.

--- example-modules/do-nothing/do_nothing.c
+++ example-modules/do-nothing/do_nothing.c
@@ -2,31 +2,31 @@
 #include "linux/module.h"
 #include "linux/kernel.h"
 
-static int clli_example_module_init(void);
-static void clli_example_module_exit(void);
+static int do_nothing_module_init(void);
+static void do_nothing_module_exit(void);
 
-static int clli_example_module_major = 60;
-static char *clli_example_module_name = "clli_example_module";
+static int do_nothing_module_major = 60;
+static char *do_nothing_module_name = "do_nothing_module";
 
 MODULE_AUTHOR("Claes Lillieskold");
 MODULE_DESCRIPTION("Claes example module");
 MODULE_LICENSE("GPL");
 
-module_param(clli_example_module_major, int, S_IRUGO);
-MODULE_PARM_DESC(clli_example_module_major, "Module template major number");
-module_param(clli_example_module_name, charp, S_IRUGO);
-MODULE_PARM_DESC(clli_example_module_name, "Module template name");
+module_param(do_nothing_module_major, int, S_IRUGO);
+MODULE_PARM_DESC(do_nothing_module_major, "Module template major number");
+module_param(do_nothing_module_name, charp, S_IRUGO);
+MODULE_PARM_DESC(do_nothing_module_name, "Module template name");
 
-module_init(clli_example_module_init);
-module_exit(clli_example_module_exit);
+module_init(do_nothing_module_init);
+module_exit(do_nothing_module_exit);
 
-static int clli_example_module_init(void)
+static int do_nothing_module_init(void)
 {
   printk(KERN_ALERT "Clli example module init.\n");
   return 0;
 }
   
-static void clli_example_module_exit(void)
+static void do_nothing_module_exit(void)
 {
 }
 
